bfc3d7964a37bf97747fc7f046a2f2da2a1ed6b0
piyan8763 <57474822+didi232345@users.noreply.github.com>
2021-05-20 13:34:01 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,22 @@
 import discord 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+
+intents = discord.Intents.default()
+intents.members = True
+
+
+bot = commands.Bot(command_prefix='[',intents = intents)
 
 @bot.event 
 async def on_ready():
     print(">>Bot is online<<")
 
-bot.run('ODQ0NjA0MjQ5Njg1MDk4NTU2.YKU1AQ.O1MNPKpayAjmquGQrTSwLwwyvbY') 
+
+@bot.event 
+async def on_member_join(member):
+    channel = bot.get_channel(825789805370540056)
+    await channel.send(f'{member} 好遮')
+    print(f'{member} 好遮')
+
+bot.run('ODQ0NjA0MjQ5Njg1MDk4NTU2.YKU1AQ.VW5NljLDv-pKicPcEPC8QO4nGzQ') 
